b0c137373240e99430384f6fb3b714eac534408c
guatajuk <sp0t.col@gmail.com>
2014-05-30 23:54:52 -0500

    Licencia MIT, primer metodo agregado, falta validacion de la entrada del menu

---
+++ main.c
@@ -0,0 +1,47 @@
+// The MIT License (MIT)
+
+// Copyright (c) YEAR NAME
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/*-------------------------------------------|
+|Julián Alberto Gaitán Arias                 |
+|Universidad de Caldas Facultad de Ingeniería|
+|Ingeniería en Sistemas y Computación        |
+|Twitter: @guatajuk                          |
+--------------------------------------------*/
+
+#include <stdio.h>
+
+int menu (){
+	int opc;
+	printf ("------------------------")
+	printf ("Arboles Binarios - Menu");
+	printf ("------------------------\n")
+	printf ("\n1. Agregar nodos a el arbol");
+	printf ("\n2. Eliminar todos los nodos del arbol");
+	printf ("\n3. Salir");
+	printf ("\tDigite su opción");
+	scanf ("%d", &opc);
+	return opc;
+}
+/* code */
+
+int main void (){
+	return 0;
+}
